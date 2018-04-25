09e64569387d9f734abc3a4e9c311b4889b1d96b
Ruben Cornejo <rubech@gmail.com>
2018-04-25 16:51:58 +0000

    NPM init

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+module.exports = Phrase;
+
 // Reverses a string.
 function reverse(string) {
   return Array.from(string).reverse().join("");
 }
-
-// Returns true for a palindrome, false otherwise.
-// function palindrome(string) {
-//   let processedContent = string.toLowerCase();
-//   return processedContent === reverse(processedContent);
-// }
 
 // Defines a Phrase object.
 function Phrase(content) {
